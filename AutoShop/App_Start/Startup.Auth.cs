e8a87dacd4066a1c3350a14cfb4a2ed38408dfde
dvg18 <reghik@yandex.ru>
2016-07-06 03:30:24 +0700

    Добавлены роли2

--- AutoShop/App_Start/Startup.Auth.cs
+++ AutoShop/App_Start/Startup.Auth.cs
@@ -7,8 +7,6 @@ using Microsoft.Owin.Security.Google;
 using Owin;
 using AutoShop.Models;
 
-[assembly: OwinStartup(typeof(AutoShop.Startup))]
-
 namespace AutoShop
 {
     public partial class Startup
@@ -21,7 +19,6 @@ namespace AutoShop
             app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
             app.CreatePerOwinContext<ApplicationSignInManager>(ApplicationSignInManager.Create);
 
-            app.CreatePerOwinContext<ApplicationRoleManager>(ApplicationRoleManager.Create);
             // Включение использования файла cookie, в котором приложение может хранить информацию для пользователя, выполнившего вход,
             // и использование файла cookie для временного хранения информации о входах пользователя с помощью стороннего поставщика входа
             // Настройка файла cookie для входа
