794f34ad8e795b55435cdcd473d68ef55c42f038
pnb3 <pnb3@10-108-02>
2016-07-05 12:44:27 +0700

    [EQ

--- AutoShop/Controllers/HomeController.cs
+++ AutoShop/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 //куча комментариев
 //большая куча комментариев
+//[thjdsdfgahklfgajkfagfyagwef8 2EFD   
 namespace AutoShop.Controllers
 {
     public class HomeController : Controller
