753426bdb0c6ac060bce10ba3cb1f7905c9f6b25
Сабыржан <@SABYRZHAN>
2016-07-05 14:33:07 +0700

    Модели

---
+++ AutoShop/Models/Employees.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AutoShop.Models
+{
+    public class Employees
+    {
+        public int id { get; set; }
+        public string fio { get; set; }
+        public string jobProfile { get; set; }
+        public string phone { get; set; }
+    }
+}
---
+++ AutoShop/Models/Price.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AutoShop.Models
+{
+    public class Price
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int cost { get; set; }
+        public string description { get; set; }
+    }
+}
